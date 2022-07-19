9edcbf281ac0f01cc381d01001ec4d80e8d546a0
a16708387 <al#&5mPKs11>
2022-07-19 12:59:41 +0700

    Add sign out item in menu

--- firebasechat/src/main/java/com/bignerdranch/android/firebasechat/MainActivity.kt
+++ firebasechat/src/main/java/com/bignerdranch/android/firebasechat/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.bignerdranch.android.firebasechat
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.firebasechat.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var auth = Firebase.auth
     private val node = "message"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,19 @@ class MainActivity : AppCompatActivity() {
         initView()
 
         onFireBaseChangeListener(getReferenceOfFireBase(node))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.sign_out) {
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initView() {
@@ -32,6 +50,8 @@ class MainActivity : AppCompatActivity() {
                 sendMessageToFireBase(getReferenceOfFireBase(node), edMessage.text.toString())
             }
         }
+
+        setUpActionBar()
     }
 
     private fun getReferenceOfFireBase(node: String): DatabaseReference {
@@ -62,5 +82,19 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun setUpActionBar() {
+        val actionBar = supportActionBar
+
+        Thread {
+            val bitMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val drawableIcon = BitmapDrawable(resources, bitMap)
+            runOnUiThread {
+                actionBar?.setDisplayHomeAsUpEnabled(true)
+                actionBar?.setHomeAsUpIndicator(drawableIcon)
+                actionBar?.title = auth.currentUser?.displayName
+            }
+        }.start()
     }
 }
